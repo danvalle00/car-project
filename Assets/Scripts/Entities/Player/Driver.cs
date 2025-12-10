62ee037600fa8a1411ca13d4e87c805e32d3534b
yhr <danieldovalle10@gmail.com>
2025-12-10 19:10:29 -0300

    Lesson 26-30

--- Assets/Scripts/Entities/Player/Driver.cs
+++ Assets/Scripts/Entities/Player/Driver.cs
@@ -1,19 +1,38 @@
+using TMPro;
 using UnityEngine;
 
 namespace Entities.Player
 {
     public class Driver : MonoBehaviour
     {
-        [SerializeField] private float steerSpeed = 1f;
-        [SerializeField] private float accelSpeed = 1f;
+        [SerializeField]
+        private float steerSpeed = 1f;
 
-        [SerializeField] private float steerInput;
-        [SerializeField] private float accelInput;
+        [SerializeField]
+        private float regularSpeed = 1f;
+        private float accelSpeed;
+
+        [SerializeField]
+        private float steerInput;
+
+        [SerializeField]
+        private float accelInput;
+
+        [SerializeField]
+        private float boostSpeed = 6f;
+
         private InputSystem_Actions _driverInput;
+        private TMP_Text infoText;
 
         private void Awake()
         {
             _driverInput = new InputSystem_Actions();
+            accelSpeed = regularSpeed;
+        }
+
+        private void Start()
+        {
+            infoText.gameObject.SetActive(false);
         }
 
         private void Update()
@@ -37,5 +56,22 @@ namespace Entities.Player
         {
             _driverInput.Driver.Disable();
         }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Boost"))
+            {
+                Debug.Log("Speed Boost!");
+                accelSpeed = boostSpeed;
+                Destroy(collision.gameObject);
+                infoText.gameObject.SetActive(true);
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            accelSpeed = regularSpeed;
+            infoText.gameObject.SetActive(false);
+        }
     }
-}
+};
--- Assets/Scripts/Entities/Player/DriverDelivery.cs
+++ Assets/Scripts/Entities/Player/DriverDelivery.cs
@@ -2,21 +2,34 @@ using UnityEngine;
 
 namespace Entities.Player
 {
+    [RequireComponent(typeof(ParticleSystem))]
     public class DriverDelivery : MonoBehaviour
     {
         private bool hasPackage;
+        private ParticleSystem packageEffect;
+
+        [SerializeField]
+        private float destroyDelay = 1f;
+
+        private void Start()
+        {
+            packageEffect = GetComponent<ParticleSystem>();
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag("Package"))
+            if (other.CompareTag("Package") && !hasPackage)
             {
-                Debug.Log("Package picked up!");
+                packageEffect.Play();
+                Destroy(other.gameObject, destroyDelay);
                 hasPackage = true;
             }
             else if (other.CompareTag("Customer") && hasPackage)
             {
                 Debug.Log("Customer served!");
                 hasPackage = false;
+                packageEffect.Stop();
+                Destroy(other.gameObject);
             }
         }
     }
